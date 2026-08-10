7039843cacc121b5206fc1841f652a06d209f1d1
Denis Alexandrovich <sdenmansss@gmail.com>
2026-08-10 22:00:20 +0500

    fix: prepared_statement_cache_size

--- core/database.py
+++ core/database.py
@@ -4,13 +4,14 @@ from core.config import settings
 
 engine = create_async_engine(
     settings.DATABASE_URL,
-    pool_size=10,
-    max_overflow=20,
+    pool_size=5,
+    max_overflow=10,
     pool_pre_ping=True,
     echo=settings.APP_ENV == "development",
     connect_args={
         "ssl": "require",
         "statement_cache_size": 0,
+        "prepared_statement_cache_size": 0,
     },
 )
 
